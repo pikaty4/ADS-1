c359e21eb4ab1e9204e3f3582e9b256f85e148fa
pikaty4 <71702322+pikaty4@users.noreply.github.com>
2022-03-20 19:30:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,34 @@
 // Copyright 2022 NNTU-CS
-#include <cstdint>
 #include "alg.h"
-
+#include <cstdint>
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+	for (uint64_t i = 2; i < value / 2 + 1; i++) {
+		if (value % i == 0) { return false; }
+	}
+	return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+	uint64_t prime = 0;
+	for (uint64_t i = 2; i <= n; i++) {
+		if (checkPrime(i)) { prime++; }
+	}
+	return prime;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+	value++;
+	while (true) {
+		if (checkPrime(value)) { return value; }
+		value++;
+	}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+	uint64_t sum = 0;
+	for (uint64_t i = 2; i < hbound; i++) {
+		if (checkPrime(i)) { sum += i; }
+	}
+	return sum;
 }
