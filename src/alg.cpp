2715b9395e84656da9488651ebc2120d16bafedc
pikaty4 <71702322+pikaty4@users.noreply.github.com>
2022-03-20 19:53:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,32 +3,32 @@
 #include <cstdint>
 
 bool checkPrime(uint64_t value) {
-	for (uint64_t i = 2; i < value / 2 + 1; i++) {
-		if (value % i == 0) { return false; }
-	}
-	return true;
+        for (uint64_t i = 2; i < value / 2 + 1; i++) {
+                if (value % i == 0) { return false; }
+        }
+        return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-	uint64_t prime = 0;
-	for (uint64_t i = 2; i <= n; i++) {
-		if (checkPrime(i)) { prime++; }
-	}
-	return prime;
+        uint64_t prime = 0;
+        for (uint64_t i = 2; i <= n; i++) {
+                if (checkPrime(i)) { prime++; }
+        }
+        return prime;
 }
 
 uint64_t nextPrime(uint64_t value) {
-	value++;
-	while (true) {
-		if (checkPrime(value)) { return value; }
-		value++;
+        value++;
+        while (true) {
+                if (checkPrime(value)) { return value; }
+                value++;
 	}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-	uint64_t sum = 0;
-	for (uint64_t i = 2; i < hbound; i++) {
-		if (checkPrime(i)) { sum += i; }
-	}
-	return sum;
+        uint64_t sum = 0;
+        for (uint64_t i = 2; i < hbound; i++) {
+                if (checkPrime(i)) { sum += i; }
+        }
+        return sum;
 }
